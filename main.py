cf3978fa69e307d9047335813df4acac38a85d47
akn714 <adarshkumar20012704@gmail.com>
2025-07-01 00:06:14 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+greet = """
+ ________       ____     _______           __            ________        ____     _______          __                    
+|_   ___ `.   .'    `.  |_   __ \         /  \          |_   ___ `.    .'    `.  |_   __ \        /  \           _       
+  | |   `. \ /  .--.  \   | |__) |       / /\ \           | |   `. \  /  .--.  \   | |__) |      / /\ \         | |      
+  | |    | | | |    | |   |  __ /       / ____ \          | |    | |  | |    | |   |  __ /      / ____ \        | |      
+ _| |___.' / \  `--'  /  _| |  \ \_   _/ /    \ \_       _| |___.' /  \  `--'  /  _| |  \ \_  _/ /    \ \_      | |      
+|________.'   `.____.'  |____| |___| |____|  |____|     |________.'    `.____.'  |____| |___||____|  |____|     |_|      
+                                                                                                                (_)      
+""".strip()
+
+os.system('clear')
+
+class GroqAI:
+    model = "llama-3.3-70b-versatile"
+
+    def __init__(self):
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+    def get_llm_response(self, messages):
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+        )
+        output = response.choices[0].message.content.strip()
+
+        if output and (output[0] == output[-1]) and output.startswith(("'", '"')):
+            output = output[1:-1]
+
+        return output
+    pass
+
+def get_prompt():
+    prompt = input('>>> ').strip()
+    return prompt
+
+llm = GroqAI()
+
+messages = [
+    {
+        'role': 'system',
+        'content': """
+        You are an helpful assistant named mini-dora, that runs on a CLI. Your name comes from a character in doremon, the mini-doremon (3 mini versions of doremon).
+        """
+    }
+]
+
+print(greet)
+
+while True:
+    prompt = get_prompt()
+    if prompt=='exit': break
+    messages.append({
+        'role': 'user',
+        'content': prompt
+    })
+    res = llm.get_llm_response(messages)
+    print('>>>', res)
+
